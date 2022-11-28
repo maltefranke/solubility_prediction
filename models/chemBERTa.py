dfed46bd3d6957190835970330a5011b5b022208
malte.franke <iz782675@login18-g-2.hpc.itc.rwth-aachen.de>
2022-11-28 15:56:27 +0100

    chemberta run augmented smiles, no weights, no downsampling

--- models/chemBERTa.py
+++ models/chemBERTa.py
@@ -4,6 +4,7 @@ from data_preparation import *
 
 from simpletransformers.classification import ClassificationModel
 import sklearn
+import torch
 
 
 def load_our_dataset(path: str, flag_test=False, augmented=False) -> Tuple[List[str], List[str], np.array]:
@@ -18,6 +19,7 @@ def load_our_dataset(path: str, flag_test=False, augmented=False) -> Tuple[List[
         return df
     else:
         df.rename(columns={'smiles': 'text', 'sol_category': 'labels'}, inplace=True)
+        df = sklearn.utils.shuffle(df)
         ind_0 = np.array(df[df['labels'] == 0].index)
         ind_1 = np.array(df[df['labels'] == 1].index)
         ind_2 = np.array(df[df['labels'] == 2].index)
@@ -27,9 +29,9 @@ def load_our_dataset(path: str, flag_test=False, augmented=False) -> Tuple[List[
         ind_valid = np.hstack([ind_0[int(0.8*ind_0.shape[0]):int(0.9*ind_0.shape[0])],
                                ind_1[int(0.8*ind_1.shape[0]):int(0.9*ind_1.shape[0])],
                                ind_2[int(0.8*ind_2.shape[0]):int(0.9*ind_2.shape[0])]])
-        ind_test = np.hstack([ind_0[int(0.9 * ind_0.shape[0]):ind_0.shape[0]],
-                              ind_1[int(0.9 * ind_1.shape[0]):ind_1.shape[0]],
-                              ind_2[int(0.9 * ind_2.shape[0]):ind_2.shape[0]]])
+        ind_test = np.hstack([ind_0[int(0.9 * ind_0.shape[0]):],
+                              ind_1[int(0.9 * ind_1.shape[0]):],
+                              ind_2[int(0.9 * ind_2.shape[0]):]])
         df_train = df.iloc[ind_train]
         df_valid = df.iloc[ind_valid]
         df_test = df.iloc[ind_test]
@@ -48,38 +50,40 @@ if __name__ == "__main__":
     train_path = os.path.join(data_dir, "augmented_smiles.csv")
     test_path = os.path.join(data_dir, "test.csv")
 
+    train_df, valid_df, test_df = load_our_dataset(train_path, flag_test=False, augmented=True)
+
     # set up a logger to record if any issues occur
     # and notify us if there are any problems with the arguments we've set for the model.
     logging.basicConfig(level=logging.INFO)
     transformers_logger = logging.getLogger("transformers")
     transformers_logger.setLevel(logging.WARNING)
 
+    use_cuda = False
+    if torch.cuda.is_available():
+        use_cuda = True
+
     model = ClassificationModel('roberta', 'seyonec/PubChem10M_SMILES_BPE_396_250', num_labels=3,
                                 # weight=sample_weights[0:int(0.8*train_data_size)],
                                 args={'evaluate_each_epoch': True,
                                       'evaluate_during_training_verbose': True,
                                       'no_save': True, 'num_train_epochs': 10,
-                                      'auto_weights': True}, use_cuda=False)
+                                      'auto_weights': True}, use_cuda=use_cuda)
 
-    train_df, valid_df, test_df = load_our_dataset(train_path, flag_test=False, augmented=True)
-
-    model.train_model(train_df, eval_df=valid_df, output_dir='outputs', multi_label=True, num_labels=3,
-                      use_cuda=False, args={'wandb_project': 'project-name'})
+    model.train_model(train_df, eval_df=valid_df, output_dir='chemBERTa_model', multi_label=True, num_labels=3,
+                      use_cuda=use_cuda)
 
     # EVALUATION OF THE MODEL - wrt a test set for which we know the true predictions
     # accuracy
     print('Accuracy score')
     result, model_outputs, wrong_predictions = model.eval_model(test_df, acc=sklearn.metrics.accuracy_score)
 
-    # ROC-PRC
-    print('Average precision score')
-    result, model_outputs, wrong_predictions = model.eval_model(test_df, acc=sklearn.metrics.average_precision_score)
-
     # Cohen-kappa evaluation
     print('Cohen-kappa metrics')
-    test_df_predictions, raw_test_df_outputs = model.predict(test_df)
-    result, model_outputs, wrong_predictions = model.eval_model(test_df, acc=sklearn.metrics.cohen_kappa_score(
-        test_df_predictions, test_df['labels'], weights='quadratic'))
+
+    test_df_predictions, raw_test_df_outputs = model.predict(test_df['text'].tolist())
+
+    kappa = sklearn.metrics.cohen_kappa_score(test_df_predictions, test_df['labels'].tolist(), weights='quadratic')
+    print(kappa)
 
     # TEST SET
     # Loading the test set
@@ -87,9 +91,9 @@ if __name__ == "__main__":
     submission_ids, submission_smiles = load_test_data(test_path)
 
     # PREDICTIONS
-    final_predictions, raw_outputs = model.predict(test_dataset)
+    final_predictions, raw_outputs = model.predict(test_dataset['text'].tolist())
 
-    submission_file = os.path.join(this_dir, "chemberta_augmented-dataset.csv")
+    submission_file = os.path.join(this_dir, "chemberta_augmented_predictions.csv")
     create_submission_file(submission_ids, final_predictions, submission_file)
 
 
