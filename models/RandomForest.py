abfadb75517edd4c41d3b4e2e5826b5bc669496f
mossinel <giacomo.mossinelli@epfl.ch>
2022-12-12 00:50:12 +0100

    update with cross validation results

--- models/RandomForest.py
+++ models/RandomForest.py
@@ -9,10 +9,19 @@ from utils import *
 from data_utils import *
 
 
-def rf_learning(X: np.array, y: np.array, CV: int = 5, depth: int = 20, label_weights: List[float] = None, seed: int = 13,
-               sample_weights: List[float] = None) \
-        -> List[RandomForestClassifier]:
-    label_weights = {0: label_weights[0], 1: label_weights[1], 2: label_weights[2]}
+def rf_learning(
+    X: np.array,
+    y: np.array,
+    CV: int = 5,
+    label_weights: List[float] = None,
+    seed: int = 13,
+    sample_weights: List[float] = None,
+) -> List[RandomForestClassifier]:
+    label_weights = {
+        0: label_weights[0],
+        1: label_weights[1],
+        2: label_weights[2],
+    }
 
     rfs = []
 
@@ -27,8 +36,13 @@ def rf_learning(X: np.array, y: np.array, CV: int = 5, depth: int = 20, label_we
         if sample_weights is not None:
             sample_weights_i = np.array(sample_weights)[train_idx]
 
-        rf = RandomForestClassifier(n_estimators=10, max_depth=depth, criterion="entropy", random_state=seed,
-                                    class_weight=label_weights)
+        rf = RandomForestClassifier(
+            n_estimators=110,
+            max_depth=20,
+            criterion="entropy",
+            random_state=seed,
+            class_weight=label_weights,
+        )
 
         rf.fit(X_train_i, y_train_i, sample_weight=sample_weights_i)
 
@@ -51,7 +65,10 @@ def predict_rf_ensemble(rfs: List[RandomForestClassifier], X) -> np.array:
     predictions = np.concatenate(predictions, axis=1)
 
     # count the number of class predictions for each sample
-    num_predicted = [np.count_nonzero(predictions == i, axis=1).reshape((-1, 1)) for i in range(3)]
+    num_predicted = [
+        np.count_nonzero(predictions == i, axis=1).reshape((-1, 1))
+        for i in range(3)
+    ]
     num_predicted = np.concatenate(num_predicted, axis=1)
 
     # majority vote for final prediction
@@ -64,50 +81,67 @@ if __name__ == "__main__":
 
     this_dir = os.path.dirname(os.getcwd())
 
-    data_dir = os.path.join(this_dir, "data")
+    data_dir = os.path.join(
+        this_dir, "solubility_prediction\data"
+    )  # MODIFY depending on your folder!!
     train_path = os.path.join(data_dir, "train.csv")
     test_path = os.path.join(data_dir, "test.csv")
 
-    # get data and transform smiles -> morgan fingerprint
+    # get data and transform smiles
     ids, smiles, targets = load_train_data(train_path)
-    all_fps = smiles_to_morgan_fp(smiles)
 
-    # GENERATE BALANCED CLASSES
-    # Up/downsampling
-    targets, all_fps = up_down_sampling(targets, all_fps)
+    # DATASET TRANSFORMATION
 
-    # see how balanced the data is and assign weights
+    # degree = (
+    #    2  # -> to be put in "preprocessing()" if you want power augmentation
+    # )
+    dataset, columns_info, log_trans = preprocessing(ids, smiles, data_dir)
     train_data_size = targets.shape[0]
 
-    num_low = np.count_nonzero(targets == 0)
-    num_medium = np.count_nonzero(targets == 1)
-    num_high = np.count_nonzero(targets == 2)
-
-    weights = [
-        1 - num_low / train_data_size,
-        1 - num_medium / train_data_size,
-        1 - num_high / train_data_size,
-    ]
-    print('The weights should be balanced now!')
-    print(weights)
-
+    # we permute/shuffle our data first
     seed = 13
     np.random.seed(seed)
-
-    # we permute/shuffle our data first
     p = np.random.permutation(targets.shape[0])
-    all_fps = all_fps[p]
+    dataset = dataset[p]
     targets = targets[p]
 
-    sample_weights = [weights[i] for i in targets]
-
-    rfs = rf_learning(all_fps, targets, CV=5, label_weights=weights, sample_weights=sample_weights, seed=seed)
+    # TEST SET
 
     submission_ids, submission_smiles = load_test_data(test_path)
-    X = smiles_to_morgan_fp(submission_smiles)
-    input_dim = X.shape[-1]
 
-    final_predictions = predict_rf_ensemble(rfs, X)
+    # TEST SET TRANSFORMATION
+    # descriptors
+    qm_descriptors_test = smiles_to_qm_descriptors(
+        submission_smiles, data_dir, "test"
+    )
 
-    submission_file = os.path.join(this_dir, "predictions.csv")
+    qm_descriptors_test, _ = transformation(
+        qm_descriptors_test,
+        columns_info,
+        standardization=True,
+        test=True,
+        degree=1,
+        pairs=False,
+        log_trans=log_trans,
+    )
+
+    # dataset, qm_descriptors_test = PCA_application(
+    #    dataset, qm_descriptors_test
+    # )
+
+    weights = calculate_class_weights(targets)
+    sample_weights = [weights[i] for i in targets]
+
+    rfs = rf_learning(
+        dataset,
+        targets,
+        CV=5,
+        label_weights=weights,
+        sample_weights=sample_weights,
+        seed=seed,
+    )
+
+    final_predictions = predict_rf_ensemble(rfs, qm_descriptors_test)
+
+    submission_file = os.path.join(this_dir, "submission.csv")
     create_submission_file(submission_ids, final_predictions, submission_file)
