d107a44d4a4d5949c8ac89a773c4ff59a23b57d7
Laura Mismetti <lauramismetti99@gmail.com>
2022-12-18 10:32:38 +0100

    doc_scaffolds

--- similarity_scaffolds.py
+++ similarity_scaffolds.py
@@ -1,11 +1,19 @@
 import numpy as np
 import pandas as pd
+import os
+import rdkit
 from rdkit.Chem.Scaffolds.MurckoScaffold import GetScaffoldForMol
-from data_utils import *
 from rdkit import DataStructs
+from rdkit import Chem
+from data_utils import load_train_data, load_test_data
 
 
 def get_scaffolds(smiles):
+    """
+    Calculates the scaffold of a list of molecules through the Murcko definition
+    :param smiles: list of SMILES
+    :return: list of respective scaffolds
+    """
     scaffold_generator = rdkit.Chem.Scaffolds.MurckoScaffold
 
     scaffolds_list = []
@@ -17,6 +25,14 @@ def get_scaffolds(smiles):
 
 
 def diversity_scores(smiles1, smiles2, threshold=0.9, no_threshold=False):
+    """
+    Calculate the similarity between two lists of SMILES
+    :param smiles1: list of SMILES
+    :param smiles2: list of SMILES
+    :param threshold: scalar value to decide if two SMILES are similar or not
+    :param no_threshold: Bool
+    :return: np.array of similarity scores
+    """
     mols1 = []
     for smile in smiles1:
         mol = Chem.MolFromSmiles(smile)
@@ -26,7 +42,6 @@ def diversity_scores(smiles1, smiles2, threshold=0.9, no_threshold=False):
         mol = Chem.MolFromSmiles(smile)
         mols2.append(mol)
 
-    # fps1 = [Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, 3, nBits=2048) for mol in mols1]
     fps2 = [Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, 3, nBits=2048) for mol in mols2]
 
     scores = np.array(
@@ -44,18 +59,30 @@ def diversity_scores(smiles1, smiles2, threshold=0.9, no_threshold=False):
 
 
 def __compute_diversity(mol, fps):
+    """
+    Calculate the similarity score for a molecule and a list of fingerprints by using Tanimoto similarity
+    :param mol: Mol object (a molecule)
+    :param fps: List of fingerprints to compare the molecule to
+    :return: mean of the similarity score between the molecule and all the fingerprints
+    """
     ref_fps = Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, 3, nBits=2048)
     # comparing a single molecule to all the others in the second group
     dist = DataStructs.BulkTanimotoSimilarity(ref_fps, fps, returnDistance=True)
-    # I get as score the average of the similarity with all the other molecules
+    # I get the average of the similarity with all the other molecules
     score = np.mean(dist)
-    #dist_a = np.array(dist)
-    #print(np.max(dist_a[dist_a < 1]))
 
     return score
 
-#########################################################################
+
 def diversity_scores_singlepair(smiles1, smiles2, threshold=0.9, no_threshold=False):
+    """
+    Calculate the similarity between two SMILES
+    :param smiles1: a SMILES
+    :param smiles2: a SMILES
+    :param threshold: scalar to determine if they are similar or not
+    :param no_threshold: Bool
+    :return: similarity score
+    """
     mol1 = Chem.MolFromSmiles(smiles1)
     mol2 = Chem.MolFromSmiles(smiles2)
     fp2 = [Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol2, 3, nBits=2048)]
@@ -66,12 +93,17 @@ def diversity_scores_singlepair(smiles1, smiles2, threshold=0.9, no_threshold=Fa
 
 
 def __compute_diversity_singlepair(mol, fp):
+    """
+    Calculate the similarity score for a molecule and a fingerprint
+    :param mol: Mol object
+    :param fp: a single fingerprint
+    :return: scalar Tanimoto similarity
+    """
     ref_fp = Chem.rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, 3, nBits=2048)
     # comparing a single molecule to all the others in the second group
     dist = DataStructs.BulkTanimotoSimilarity(ref_fp, fp, returnDistance=True)
 
     return dist
-#########################################################################
 
 
 class Scaffold:
@@ -94,10 +126,6 @@ class Scaffold:
 
     def __len__(self):
         return self.num_mol
-
-    # def __get_item__(self):
-    #     obj_list = list(self.smiles, self.num_mol, self.indices, self.num_0, self.num_1, self.num_2)
-    #     return obj_list
 
     def __add__(self, ind, target):
         self.num_mol += 1
@@ -129,6 +157,16 @@ class Scaffold:
 
 
 def group_scaffolds(ids, scaffolds, targets, path):
+    """
+    Creation of a dictionary (and production of a .csv file) that contains all the unique sccaffolds of the train set,
+    and the respective data
+    :param ids: list of Ids of the molecules
+    :param scaffolds: list of SMILES of the scaffolds
+    :param targets: list of targets
+    :param path: destination path where to save the .csv file
+    :return: dictionary with all the unique scaffolds, the number of molecules with that same scaffold,
+             their ids, the number of them that belong to class 0, 1 and 2
+    """
     # Creating a dictionary with all the scaffolds
     dict_scaffolds = {}
 
@@ -188,10 +226,6 @@ class ScaffoldTest:
     def __len__(self):
         return self.num_mol
 
-    # def __get_item__(self):
-    #     obj_list = list(self.smiles, self.num_mol, self.indices)
-    #     return obj_list
-
     def __add__(self, ind):
         self.num_mol += 1
         self.indices.append(ind)
@@ -207,10 +241,19 @@ class ScaffoldTest:
 
 
 def group_scaffolds_test(ids, scaffolds, path):
+    """
+    Creation of a dictionary (and production of a .csv file) that contains all the unique sccaffolds of the TEST set,
+    and the respective data
+    :param ids: list of Ids of the molecules
+    :param scaffolds: list of SMILES of the scaffolds
+    :param path: destination path where to save the .csv file
+    :return: dictionary with all the unique scaffolds, the number of molecules with that same scaffold,
+             their ids
+    """
     # Creating a dictionary with all the scaffolds
     dict_scaffolds = {}
 
-    for (i, item, t) in zip(ids, scaffolds, targets):
+    for (i, item) in zip(ids, scaffolds):
         if item in dict_scaffolds:
             # identical scaffold
             dict_scaffolds[item].__add__(i)
@@ -251,9 +294,13 @@ def group_scaffolds_test(ids, scaffolds, path):
     return dict_scaffolds
 
 
-# Comparison scaffolds train and test sets
 def comparison_scaffolds(dict_scaffolds, dict_scaffolds_test):
-    # check if there are the same scaffolds in train and test sets
+    """
+    Check if there are the same scaffolds in train and test sets
+    :param dict_scaffolds: dictionary with unique scaffolds in the train set
+    :param dict_scaffolds_test: dictionary with unique scaffolds in the test set
+    :return: number of similar scaffolds
+    """
     count = 0
     for item_test in dict_scaffolds_test:
         for item_train in dict_scaffolds:
@@ -287,73 +334,18 @@ if __name__ == "__main__":
     ids, smiles, targets = load_train_data(train_path)
     scaffolds = get_scaffolds(smiles)
     print('Calculated scaffolds')
-    path = data_dir + 'grouped_scaffolds.csv'
+    path = os.path.join(data_dir, 'grouped_scaffolds.csv')
     dict_train = group_scaffolds(ids, scaffolds, targets, path)
 
     print('TEST SET')
     ids_test, smiles_test = load_test_data(test_path)
     scaffolds_test = get_scaffolds(smiles_test)
     print('Calculated scaffolds')
-    path_test = data_dir + 'grouped_scaffolds_test.csv'
+    path_test = os.path.join(data_dir, 'grouped_scaffolds_test.csv')
     dict_test = group_scaffolds_test(ids_test, smiles_test)
 
-    # comparison train and test
+    # COMPARISON TRAIN AND TEST
     count = comparison_scaffolds(dict_train, dict_test)
-
-    # SIMILARITY
-    # we work only with smiles, not with MorganFP in the main
-    # ids, smiles, targets = load_train_data(train_path)
-
-    # print('******** TRAIN SET ANALYSIS ********')
-    # # ********* SIMILARITY TRAIN SET **********
-    # print('Similarity of the whole train set')
-    # scores = diversity_scores(smiles, smiles)
-    #
-    # # ********* SCAFFOLDS STUDY **********
-    # # Get the scaffolds of the whole train set
-    # scaffolds = get_scaffolds(smiles)
-    # print('Similarity of the scaffolds of the whole TRAIN SET')
-    # scores_scaffolds = diversity_scores(scaffolds, scaffolds)
-    #
-    # # STUDY HOW THE SCAFFOLDS OF THE DIFFERENT CLASSES ARE
-    # # look for some similarity
-    # # divide the train set in classes
-    # smiles2 = np.array(smiles)[np.where(targets == 2)[0]]
-    # scaffolds2 = get_scaffolds(smiles2)
-    # smiles1 = np.array(smiles)[np.where(targets == 1)[0]]
-    # scaffolds1 = get_scaffolds(smiles1)
-    # smiles0 = np.array(smiles)[np.where(targets == 0)[0]]
-    # scaffolds0 = get_scaffolds(smiles0)
-    #
-    # # There are some identical scaffolds??
-    #
-    # # compute similarity of scaffolds (smiles) in each class
-    # print('Similarity of scaffolds of the same class (train set)')
-    # scores0 = diversity_scores(scaffolds0, scaffolds0)
-    # scores1 = diversity_scores(scaffolds1, scaffolds1)
-    # scores2 = diversity_scores(scaffolds2, scaffolds2)
-    #
-    # # REPEAT THE SAME PROCESS FOR THE TEST SET
-    # print('******** TEST SET ANALYSIS ********')
-    # ids, smiles_test = load_test_data(test_path)
-    #
-    # print('Similarity of the whole test set')
-    # scores_test = diversity_scores(smiles_test, smiles_test)
-    #
-    # # ******** SCAFFOLD TEST STUDY ********
-    # scaffolds_test = get_scaffolds(smiles_test)
-    # print('Similarity of the scaffolds of the whole TEST SET')
-    # scores_test = diversity_scores(scaffolds_test, scaffolds_test)
-    #
-    # # Now I can try to understand if by looking at the scaffolds of the test set
-    # # I can find some similarities with the scaffolds of each class
-    # # COMPARISON OF THE TEST SCAFFOLDS WITH EACH CLASS
-    # # I need a different version of the similarity functions
-    # print('Comparison of the TEST SET with the classes of the train set')
-    #
-    # scores_test0 = diversity_scores(scaffolds_test, scaffolds0)
-    # scores_test1 = diversity_scores(scaffolds_test, scaffolds1)
-    # scores_test2 = diversity_scores(scaffolds_test, scaffolds2)
 
 
 
